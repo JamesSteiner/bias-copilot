71bc06e2dfa8962511d5216db0f7f2f993c0e94b
Bence Hervay <bh525@cam.ac.uk>
2023-02-07 13:16:50 +0000

    GPT works with fetch

--- ai_copilot/gpt3-api/fetch.js
+++ ai_copilot/gpt3-api/fetch.js
@@ -1,34 +1,36 @@
 function OpenaiFetchAPI() {
-  console.log("Calling GPT3");
-  var url = "https://api.openai.com/v1/engines/davinci/completions";
-  var bearer = "Bearer " + YOUR_TOKEN;
-  fetch(url, {
-    method: "POST",
-    headers: {
-      Authorization: bearer,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      prompt: "Once upon a time",
-      max_tokens: 5,
-      temperature: 1,
-      top_p: 1,
-      n: 1,
-      stream: false,
-      logprobs: null,
-      stop: "\n",
-    }),
-  })
-    .then((response) => {
-      return response.json();
+    console.log("Calling GPT3");
+    var url = "https://api.openai.com/v1/engines/davinci/completions";
+    var bearer = "Bearer " + YOUR_TOKEN;
+    fetch(url, {
+        method: "POST",
+        headers: {
+            Authorization: bearer,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            prompt: "Once upon a time",
+            max_tokens: 10,
+            temperature: 0.7,
+            top_p: 1,
+            n: 1,
+            stream: false,
+            logprobs: null,
+            stop: "\n",
+        }),
     })
-    .then((data) => {
-      console.log(data);
-      console.log(typeof data);
-      console.log(Object.keys(data));
-      console.log(data["choices"][0].text);
-    })
-    .catch((error) => {
-      console.log("Something bad happened " + error);
-    });
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            console.log(typeof data);
+            console.log(Object.keys(data));
+            console.log(data["choices"][0].text);
+        })
+        .catch((error) => {
+            console.log("Something bad happened " + error);
+        });
 }
+
+module.exports = {OpenaiFetchAPI};
--- ai_copilot/main.js
+++ ai_copilot/main.js
@@ -1,7 +1,46 @@
+//import {OpenaiFetchAPI} from "./gpt3-api/fetch";
+
 define([
     'base/js/namespace',
     'base/js/events'
 ], function (Jupyter, events) {
+
+    let TOKEN = "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"
+
+    function OpenaiFetchAPI() {
+        console.log("Calling GPT3");
+        var url = "https://api.openai.com/v1/engines/davinci/completions";
+        var bearer = "Bearer " + TOKEN;
+        fetch(url, {
+            method: "POST",
+            headers: {
+                Authorization: bearer,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                prompt: "Once upon a time",
+                max_tokens: 5,
+                temperature: 1,
+                top_p: 1,
+                n: 1,
+                stream: false,
+                logprobs: null,
+                stop: "\n",
+            }),
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                console.log(typeof data);
+                console.log(Object.keys(data));
+                console.log(data["choices"][0].text);
+            })
+            .catch((error) => {
+                console.log("Something bad happened " + error);
+            });
+    }
 
     function get_selected_codes() {
         let cells = Jupyter.notebook.get_selected_cells();
@@ -23,6 +62,8 @@ define([
             bias_count += (codes[i].match(/bias/g) || []).length;
         }
         let content = "#  Bias count: " + bias_count;
+
+        OpenaiFetchAPI();
 
 
         Jupyter.notebook.insert_cell_below('code').set_text(content);
