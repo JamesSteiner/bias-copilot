58edbacf3f982fddc7e9365f9b3dd7c2961ee58e
Lei QiaoZhi <georgeleiqiaozhi@gmail.com>
2023-02-09 10:42:46 +0000

    updated fetch api and created a test html

--- ai_copilot/gpt3-api/fetch.js
+++ ai_copilot/gpt3-api/fetch.js
@@ -1,36 +1,36 @@
-function OpenaiFetchAPI() {
-    console.log("Calling GPT3");
-    var url = "https://api.openai.com/v1/engines/davinci/completions";
-    var bearer = "Bearer " + YOUR_TOKEN;
+function OpenaiFetchAPI(prompt, max_tokens = 10, temp = 0.7) {
+  console.log("Calling GPT3");
+  var url = "https://api.openai.com/v1/engines/davinci/completions";
+  var bearer = "Bearer " + TOKEN;
+  return (
     fetch(url, {
-        method: "POST",
-        headers: {
-            Authorization: bearer,
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            prompt: "Once upon a time",
-            max_tokens: 10,
-            temperature: 0.7,
-            top_p: 1,
-            n: 1,
-            stream: false,
-            logprobs: null,
-            stop: "\n",
-        }),
+      method: "POST",
+      headers: {
+        Authorization: bearer,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        max_tokens: max_tokens,
+        temperature: temp,
+        top_p: 1,
+        n: 1,
+        stream: false,
+        logprobs: null,
+        stop: "\n",
+      }),
     })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            console.log(typeof data);
-            console.log(Object.keys(data));
-            console.log(data["choices"][0].text);
-        })
-        .catch((error) => {
-            console.log("Something bad happened " + error);
-        });
+      .then((response) => {
+        return response.json();
+      })
+      // .then((data) => {
+      //   console.log(data);
+      //   console.log(typeof data);
+      //   console.log(Object.keys(data));
+      //   console.log(data["choices"][0].text);
+      // })
+      .catch((error) => {
+        console.log("Something bad happened " + error);
+      })
+  );
 }
-
-module.exports = {OpenaiFetchAPI};
