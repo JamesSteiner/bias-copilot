aca9d4ee0b8e07a26eb0ff0788793668aa39e47d
Lei QiaoZhi <georgeleiqiaozhi@gmail.com>
2023-02-09 10:46:09 +0000

    moved node files into a separate folder
    can prob delete later

--- ai_copilot/gpt3-api/fetch.js
+++ ai_copilot/gpt3-api/fetch.js
@@ -2,35 +2,27 @@ function OpenaiFetchAPI(prompt, max_tokens = 10, temp = 0.7) {
   console.log("Calling GPT3");
   var url = "https://api.openai.com/v1/engines/davinci/completions";
   var bearer = "Bearer " + TOKEN;
-  return (
-    fetch(url, {
-      method: "POST",
-      headers: {
-        Authorization: bearer,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        prompt: prompt,
-        max_tokens: max_tokens,
-        temperature: temp,
-        top_p: 1,
-        n: 1,
-        stream: false,
-        logprobs: null,
-        stop: "\n",
-      }),
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: bearer,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+      max_tokens: max_tokens,
+      temperature: temp,
+      top_p: 1,
+      n: 1,
+      stream: false,
+      logprobs: null,
+      stop: "\n",
+    }),
+  })
+    .then((response) => {
+      return response.json();
     })
-      .then((response) => {
-        return response.json();
-      })
-      // .then((data) => {
-      //   console.log(data);
-      //   console.log(typeof data);
-      //   console.log(Object.keys(data));
-      //   console.log(data["choices"][0].text);
-      // })
-      .catch((error) => {
-        console.log("Something bad happened " + error);
-      })
-  );
+    .catch((error) => {
+      console.log("Something bad happened " + error);
+    });
 }
