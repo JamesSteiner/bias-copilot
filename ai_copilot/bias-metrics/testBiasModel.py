b077b9e2d9f88b204b9d3a34a0dff669e80d8e38
Zixi Zhang <zz458@cam.ac.uk>
2023-02-16 13:13:35 +0000

    Bias testing for user's model: back-end done: encapsulate FairnessDashboard

---
+++ ai_copilot/bias-metrics/testBiasModel.py
@@ -0,0 +1,17 @@
+from typing import List, Union, Dict
+import numpy as np
+import pandas as pd
+
+
+def test_bias_model(dataset: Union[Union[np.ndarray, Dict[str, List]], pd.DataFrame],
+                    sensitive_attr_names: List[str],
+                    y_true: Union[List, np.ndarray],
+                    y_pred: Union[Union[List, np.ndarraym], Dict[str, List]]):
+    data = pd.DataFrame(dataset)
+    sensitive_attrs = data[sensitive_attr_names]
+    print('Click on the link below to open the dashboard:')
+
+    from raiwidgets import FairnessDashboard
+    FairnessDashboard(sensitive_features=sensitive_attrs,
+                      y_true=y_true,
+                      y_pred=y_pred)
