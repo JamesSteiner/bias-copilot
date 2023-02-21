de1c5d078be9f1da5515b80cc079eb8278fb13e5
Lei QiaoZhi <georgeleiqiaozhi@gmail.com>
2023-02-21 12:05:13 +0000

    moved info to a separate file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+# /Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages/jupyter_contrib_nbextensions/nbextensions
 jupyter nbextension install ai_copilot 
 jupyter nbextension enable ai_copilot/main 
+jupyter nbextension enable ai_copilot/info
 jupyter notebook
