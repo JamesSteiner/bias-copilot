0c9a1eef9a5d4e2c6ec847430036ba0afe9b879e
Lei QiaoZhi <georgeleiqiaozhi@gmail.com>
2023-02-23 12:06:25 +0000

    Remove API key from notebook

--- ai_copilot/main.js
+++ ai_copilot/main.js
@@ -36,7 +36,10 @@ define(["base/js/namespace", "base/js/events"], function (Jupyter, events) {
       Jupyter.notebook.select_prev();
       Jupyter.notebook
         .insert_cell_above()
-        .set_text("# Paste you API key below.");
+        .set_text(
+          "# Paste you API key below.\n" +
+            "#You can find your API key at https://platform.openai.com/account/api-keys.\n"
+        );
     }
   }
 
